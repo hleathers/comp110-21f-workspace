33c1334e213d1470fd0e5bc3d8ad098198e5c016
Hannah Leathers <hannahleathers246@unc.edu>
2021-09-26 16:55:14 -0400

    Progress on exercise 3

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,3 +4,10 @@ __author__ = "123456789"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+i: int = 0
+while i < len(TREE):
+    j: int = i + 0
+    while j < len(TREE):
+        print(TREE[j])
+        j += 1
+    i += 1
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,17 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730407726"
 
 
 # Begin your solution here...
+number: int = int(input("Type a number."))
+if number % 2 == 0 and number % 7 == 0:
+    print("TAR HEELS")
+else:
+    if number % 2 == 0:
+        print("TAR")
+    else: 
+        if number % 7 == 0:
+            print("HEELS")
+        else:
+            print("CAROLINA")
