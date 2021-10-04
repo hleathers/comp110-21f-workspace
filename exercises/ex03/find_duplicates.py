e901a54beebfbdd0554e91e33f3121e091f9ab25
Hannah Leathers <hannahleathers246@unc.edu>
2021-10-04 16:56:33 -0400

    progress ex3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -2,13 +2,16 @@
 
 __author__ = "730407726"
 
-word: str = input("Enter a word.")
+word: str = input("Enter a word: ")
 
 i: int = 0
+f: bool = False
 while i < len(word):
-    j: int = i + 0
+    j: int = i + 1
     while j < len(word):
-        print(word[j])
+        if word[i] == word[j]:
+            f = True
         j += 1
     i += 1
-    
+
+print("Found duplicate: " + str(f))
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -4,14 +4,15 @@ __author__ = "730407726"
 
 
 # Begin your solution here...
+
 number: int = int(input("Type a number."))
-if number % 2 == 0 and number % 7 == 0:
-    print("TAR HEELS")
-else:
-    if number % 2 == 0:
-        print("TAR")
-    else: 
-        if number % 7 == 0:
-            print("HEELS")
+if number % 2 == 0:
+    print("TAR")
+else: 
+    if number % 7 == 0:
+        print("HEELS")
+    else:
+        if number % 2 == 0 and number % 7 == 0:
+            print("TAR HEELS")
         else:
             print("CAROLINA")
