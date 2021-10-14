880f40e9bae0d5faed0e76432f911c702844a419
Hannah Leathers <hannahleathers246@unc.edu>
2021-10-14 13:23:32 -0400

    ex01

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,11 +1,11 @@
 """This is the numeric operator assignment."""
 __author__ = "730407726"
 
-leftside: int = int(input("Enter a number:"))
-rightside: int = int(input("Enter another number:"))
+leftside: int = int(input("Left-hand side: "))
+rightside: int = int(input("Right-hand side: "))
 
 
-print(leftside ** rightside)
-print(leftside / rightside)
-print(leftside // rightside)
-print(leftside % rightside)
+print(leftside, "**", rightside, "is", leftside ** rightside)
+print(leftside, "/", rightside, "is", leftside / rightside)
+print(leftside, "//", rightside, "is", leftside // rightside)
+print(leftside, "%", rightside, "is", leftside % rightside)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,10 +1,10 @@
 """This is the relational operators exercise."""
 __author__ = "730407726"
 
-leftside: int = int(input("Enter a number:"))
-rightside: int = int(input("Enter another number:"))
+leftside: int = int(input("Left-hand side: "))
+rightside: int = int(input("Right-hand side: "))
 
-print(leftside < rightside, "is False")
-print(leftside >= rightside, "is True")
-print(leftside == rightside, "is False")
-print(leftside != rightside, "is True")
+print(leftside, "<", rightside, "is", leftside < rightside)
+print(leftside, ">=", rightside, "is", leftside >= rightside)
+print(leftside, "==", rightside, "is", leftside == rightside)
+print(leftside, "!=", rightside, "is", leftside != rightside)
