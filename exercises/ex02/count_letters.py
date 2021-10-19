be773f44f76a6c9bf835c5499ba474c8fddc26ad
Hannah Leathers <hannahleathers246@unc.edu>
2021-10-19 13:00:38 -0400

    ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,9 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730407726"
 
 
 # Begin your solution here...
+input("What letter do you want to search for?: ")
+input("Enter a word: ")
+print("Count:")
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,3 +1,10 @@
 """Repeating a beat in a loop."""
 __author__ = "730407726"
+
 # Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")
+repeat: int = (input("How many times do you want to repeat it?")
+
+while beat >= 1:
+    return(beat[repeat])
+    
